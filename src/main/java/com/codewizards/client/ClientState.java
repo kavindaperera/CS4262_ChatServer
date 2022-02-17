2bb42f1bae25b0a66c2a295b97e367eb2b75eedb
Minul Lamahewage <minullamahewage@gmail.com>
2022-02-17 13:49:11 +0530

    Add implementation to `CientState` class

--- src/main/java/com/codewizards/client/ClientState.java
+++ src/main/java/com/codewizards/client/ClientState.java
@@ -1,11 +1,42 @@
 package com.codewizards.client;
 
+import java.net.Socket;
+
 public class ClientState {
 
-    // Id
+    private String roomId;
 
-    // RoomId
+    private String clientId;
 
-    // socket
+    private Socket socket;
 
+    public ClientState(String roomId, String clientId, Socket socket) {
+        this.roomId = roomId;
+        this.clientId = clientId;
+        this.socket = socket;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
 }
