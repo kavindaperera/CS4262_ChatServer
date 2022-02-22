7c7c7b7fc7a71eb8198caca36e5345ddd97de098
kavindaperera <perera.kavinda52@gmail.com>
2022-02-22 15:37:35 +0530

    Add `Server` class

--- src/main/java/com/codewizards/server/Server.java
+++ src/main/java/com/codewizards/server/Server.java
@@ -1,9 +1,26 @@
 package com.codewizards.server;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Server {
 
+    @Getter @Setter
+    private String serverId;
 
+    @Getter @Setter
+    private String serverAddress;
 
+    @Getter @Setter
+    private int clientPort;
 
+    @Getter @Setter
+    private int coordinationPort;
 
+    public Server(String serverId, String serverAddress, int clientPort, int coordinationPort) {
+        this.serverId = serverId;
+        this.serverAddress = serverAddress;
+        this.clientPort = clientPort;
+        this.coordinationPort = coordinationPort;
+    }
 }
