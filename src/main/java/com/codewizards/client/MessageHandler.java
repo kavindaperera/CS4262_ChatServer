a2c04d7bafcacb8f2ecbb4ae871e5589c42c9662
JudeRanidu <juderanidu96@gmail.com>
2022-02-27 22:36:30 +0530

    Modify `requestClientIdApproval` & `approveClientId` type message handling

--- src/main/java/com/codewizards/client/MessageHandler.java
+++ src/main/java/com/codewizards/client/MessageHandler.java
@@ -1,16 +1,36 @@
 package com.codewizards.client;
 
+import com.codewizards.Constants;
 import com.codewizards.message.ClientMessage;
+import com.codewizards.message.ServerMessage;
 import com.codewizards.room.RoomManager;
+import com.codewizards.server.Server;
+import com.codewizards.server.ServerState;
+import io.reactivex.Completable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableCompletableObserver;
+import lombok.NonNull;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class MessageHandler {
 
     public static Logger logger = Logger.getLogger(MessageHandler.class.getName());
+
+    private Disposable idApprovalTimeoutDisposable;
+
+    private AtomicBoolean isWaitingForIdApproval = new AtomicBoolean(false);
+
+    private AtomicBoolean idApprovalReceived = new AtomicBoolean(false);
 
     //createRoom
 
@@ -24,7 +44,11 @@ public class MessageHandler {
         String identity = (String) receivedMessage.get("identity");
         logger.info("Client requested for identity " + identity);
 
-        // check with leader for the availability and send response
+        /*
+        sendRequestApprovalMessage(ServerState.getInstance().getCoordinator(), identity);
+        startIdApprovalTimeout();
+        JSONObject response = ClientMessage.getAllowNewIdentityResponse((idApprovalReceived.toString());
+        */
 
         JSONObject response = ClientMessage.getAllowNewIdentityResponse("true");
 
@@ -65,6 +89,57 @@ public class MessageHandler {
         JSONObject broadcastMessage = ClientMessage.getMessageBroadcast(clientId, content);
 
         return broadcastMessage;
+    }
+
+
+
+    private void sendRequestApprovalMessage(Server server, String identity) {
+        logger.info("Send requestClientIdApproval to: " + server.getServerId());
+        try {
+            Socket socket = new Socket(server.getServerAddress(), server.getCoordinationPort());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.write((ServerMessage.getRequestClientIdApprovalMessage(ServerState.getInstance().getOwnServer().getServerId(), identity) + "\n").getBytes(StandardCharsets.UTF_8));
+            dataOutputStream.flush();
+
+        } catch (IOException e) {
+            logger.error(e.getLocalizedMessage() + ": " + server.getServerId());
+        }
+    }
+
+    private void startIdApprovalTimeout() {
+        idApprovalTimeoutDisposable = (Completable.timer(Constants.REQUEST_APPROVAL_TIMEOUT, TimeUnit.MILLISECONDS)
+                .subscribeWith(new DisposableCompletableObserver() {
+                                   @Override
+                                   public void onStart() {
+                                       logger.info("request identity approval timeout started!");
+                                       isWaitingForIdApproval.set(true);
+                                   }
+
+                                   @Override
+                                   public void onError(@NonNull Throwable error) {
+                                       error.printStackTrace();
+                                   }
+
+                                   @Override
+                                   public void onComplete() {
+                                       logger.info("request identity approval timeout completed!");
+                                       if (!idApprovalReceived.get()) {
+                                           logger.info("Started a new Election");
+                                           //start new election
+                                       }
+                                       stopIdApprovalTimeout();
+                                   }
+                               }
+                )
+        );
+    }
+
+    public void stopIdApprovalTimeout() {
+        if (isWaitingForIdApproval.get() && !idApprovalTimeoutDisposable.isDisposed()) {
+            logger.info("request identity approval timeout stopped!");
+            isWaitingForIdApproval.set(false);
+            idApprovalTimeoutDisposable.dispose();
+        }
     }
 
 }
