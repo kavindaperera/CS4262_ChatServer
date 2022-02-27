b90750827ce1705dd08fa4331e16a659e491ee2c
JudeRanidu <juderanidu96@gmail.com>
2022-02-27 22:40:16 +0530

    Add tiimeout for request approval

--- src/main/java/com/codewizards/Constants.java
+++ src/main/java/com/codewizards/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
     public static final long VIEW_MESSAGE_TIMEOUT = 8000L;      // T2
     public static final long ANSWER_MESSAGE_TIMEOUT = 6000L;    // T2
     public static final long HEARTBEAT_SND_INTERVAL = 3000L;
+    public static final long REQUEST_APPROVAL_TIMEOUT = 8000L;
 
 }
