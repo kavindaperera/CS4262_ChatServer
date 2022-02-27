2fbe1d891ab623eb302a2140d16c8a65536953e7
kavindaperera <perera.kavinda52@gmail.com>
2022-02-27 20:16:18 +0530

    Start feature leader `heartbeat`

--- src/main/java/com/codewizards/Constants.java
+++ src/main/java/com/codewizards/Constants.java
@@ -2,8 +2,9 @@ package com.codewizards;
 
 public class Constants {
 
+    // timeouts in milliseconds
     public static final long VIEW_MESSAGE_TIMEOUT = 8000L;      // T2
     public static final long ANSWER_MESSAGE_TIMEOUT = 6000L;    // T2
-    public static final long HEARTBEAT_SND_INTERVAL = 6000L;    // T2
+    public static final long HEARTBEAT_SND_INTERVAL = 3000L;
 
 }
---
+++ src/main/java/com/codewizards/election/Leader.java
@@ -0,0 +1,91 @@
+package com.codewizards.election;
+
+import com.codewizards.Constants;
+import com.codewizards.server.Server;
+import com.codewizards.server.ServerState;
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Leader {
+
+    public static Logger logger = Logger.getLogger(Leader.class.getName());
+
+    private static Leader INSTANCE;
+
+    private static Observable<Server> serverObservable;
+
+    private static AtomicBoolean heartbeatRunning = new AtomicBoolean(false);
+
+    private Leader() {
+
+    }
+
+    public static synchronized Leader getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Leader();
+        }
+        return INSTANCE;
+    }
+
+    public static synchronized void deleteInstance() {
+        heartbeatRunning.set(false);
+        serverObservable = null;
+        INSTANCE = null;
+        logger.info("Leader Heartbeat Stopped");
+    }
+
+    public void startHeartbeat() {
+
+        if (serverObservable != null) return;
+
+        List<Server> servers = ServerState.getInstance().getServerListAsArrayList();
+
+        serverObservable = Observable
+                .create((ObservableOnSubscribe<Server>) emitter -> {
+                    for (Server server : servers) {
+                        if (!emitter.isDisposed()) {
+                            emitter.onNext(server);
+                        }
+                    }
+                    if (!emitter.isDisposed()) {
+                        emitter.onComplete();
+                    }
+                }).subscribeOn(Schedulers.io())
+                .repeatWhen(throwableObservable -> throwableObservable.delay(Constants.HEARTBEAT_SND_INTERVAL, TimeUnit.MILLISECONDS))
+                .filter(server -> heartbeatRunning.get());
+
+        serverObservable.subscribe(new Observer<Server>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable disposable) {
+                logger.info("onSubscribe: Starting Leader Heartbeat");
+                heartbeatRunning.set(true);
+            }
+
+            @Override
+            public void onNext(@NonNull Server server) {
+                logger.info("onNext: Send Leader Heartbeat to " + server.getServerId());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable throwable) {
+                logger.error("onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                logger.info("onComplete: ");
+            }
+        });
+
+    }
+
+}
