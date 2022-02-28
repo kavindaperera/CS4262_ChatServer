8ec4e9b86456b64da0e6b72f3f336526c998d832
Hiran Lowe <w.n.r.lowe@gmail.com>
2022-02-28 23:09:19 +0530

    Add heartbeat wait timeout

--- src/main/java/com/codewizards/Constants.java
+++ src/main/java/com/codewizards/Constants.java
@@ -5,7 +5,8 @@ public class Constants {
     // timeouts in milliseconds
     public static final long VIEW_MESSAGE_TIMEOUT = 8000L;      // T2
     public static final long ANSWER_MESSAGE_TIMEOUT = 6000L;    // T2
-    public static final long HEARTBEAT_SND_INTERVAL = 3000L;
+    public static final long HEARTBEAT_SND_INTERVAL = 2000L;
     public static final long REQUEST_APPROVAL_TIMEOUT = 8000L;
+    public static final long HEARTBEAT_WAIT_INTERVAL = HEARTBEAT_SND_INTERVAL * 2;
 
 }
