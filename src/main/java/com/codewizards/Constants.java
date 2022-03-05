2f03d012fad6aefd915e65a4a827adb5588fae36
kavindaperera <perera.kavinda52@gmail.com>
2022-03-05 11:50:46 +0530

    Fix `view` message timeout behaviour

--- src/main/java/com/codewizards/Constants.java
+++ src/main/java/com/codewizards/Constants.java
@@ -3,9 +3,10 @@ package com.codewizards;
 public class Constants {
 
     // timeouts in milliseconds
-    public static final long VIEW_MESSAGE_TIMEOUT = 6000L;      // T2
-    public static final long ANSWER_MESSAGE_TIMEOUT = 6000L;    // T2
-    public static final long HEARTBEAT_SND_INTERVAL = 5000L;
+    public static final long VIEW_MESSAGE_TIMEOUT = 6000L;              // T2
+    public static final long ANSWER_MESSAGE_TIMEOUT = 6000L;            // T2
+    public static final long COORDINATOR_MESSAGE_TIMEOUT = 7000L;       // T3
+    public static final long HEARTBEAT_SND_INTERVAL = 10000L;
     public static final long REQUEST_APPROVAL_TIMEOUT = 8000L;
     public static final long HEARTBEAT_WAIT_INTERVAL = HEARTBEAT_SND_INTERVAL * 2;
 
