11b6c8788f970f9405c8b0a1bd88b8935b81121d
Minul Lamahewage <minullamahewage@gmail.com>
2022-03-09 21:48:45 +0530

    Improve handle server failue

--- src/main/java/com/codewizards/server/ServerHandler.java
+++ src/main/java/com/codewizards/server/ServerHandler.java
@@ -105,6 +105,7 @@ public class ServerHandler extends Thread{
                 }
                 case "informServerFailure":{
                     logger.debug("Received informServerFailure message: " + server.getServerId());
+                    ServerState.getInstance().removeServerFromServerView(server);
                     // TODO -  handle server failure
                     break;
                 }
